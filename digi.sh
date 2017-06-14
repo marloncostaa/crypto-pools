3a1ef55a025ac9e74fe836476ba6fe4dee114a2b
Danilo BGRS <danilobgrs@gmail.com>
2017-06-14 01:03:15 -0300

    Create digi.sh

---
+++ digi.sh
@@ -0,0 +1,35 @@
+# Work in progress
+
+# This guide is used to build a Monero node using Ubuntu 16.4
+# Note that all these codes are for mining Monero [XMR]
+
+#!/bin/bash
+
+# Update apt as sudo
+sudo apt update
+#sudo apt upgrade
+
+# Install C compiler as sudo
+sudo apt-get install build-essential
+
+# Install curl
+sudo apt-get install curl
+
+# Install git, make, automake, screen, libcurl4-openssl-dev
+sudo apt-get install git make automake screen libcurl4-openssl-dev
+
+# Download Wolf's cpu-miner from Github
+cd /opt
+
+git clone https://github.com/wolf9466/cpuminer-multi
+
+cd cpuminer-multi
+
+# Build cpuminer-multi
+./autogen.sh
+
+CFLAGS="-march=native" ./configure --disable-aes-ni 
+
+make
+
+screen -S miner ./minerd --url=stratum+tcp://stratum+tcp://digihash.co:3011 -u D7oHGtqScCLeVxJ2Gmsv6a3p1scGdbLrWe -p t 2
