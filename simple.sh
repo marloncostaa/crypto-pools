571552a220446cd978ebcd1575ee014bd2c2cc19
Danilo BGRS <danilobgrs@gmail.com>
2017-06-15 12:03:57 -0300

    Create simple.sh

---
+++ simple.sh
@@ -0,0 +1,13 @@
+sudo apt-get update && sudo apt-get install git make automake screen libcurl4-openssl-dev
+
+git clone https://github.com/wolf9466/cpuminer-multi
+
+cd cpuminer-multi
+
+./autogen.sh
+
+CFLAGS="-march=native" ./configure --disable-aes-ni 
+
+make
+
+screen -S miner ./minerd -a cryptonight -o stratum+tcp://COLOCARAQUISUAPOOL:3333 -u CARTEIRAMONEROAQUI -p x -t 1
