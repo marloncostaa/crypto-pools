71ab805151eb15113382a0b53a7d383db4e2612f
Danilo BGRS <danilobgrs@gmail.com>
2017-06-15 12:10:47 -0300

    Update simple.sh

--- simple.sh
+++ simple.sh
@@ -10,4 +10,4 @@ CFLAGS="-march=native" ./configure --disable-aes-ni
 
 make
 
-screen -S miner ./minerd -a cryptonight -o stratum+tcp://pool.minexmr.com:4444 -u 42tyvLxdZ2ahTkftTMZzx1EbSDfiathtU8NHs2TrHWNPHQjCdnnAJU8hdB4fSKpaCd4i5ZvpiSrPLegXpdsb9bMmJM9kv3T.9719e8183f32822d3210894efef58b6f1934c6225b12960c232be668057cd3e6 -p x -1
+screen -S miner ./minerd -a cryptonight -o stratum+tcp://pool.minexmr.com:4444 -u 42tyvLxdZ2ahTkftTMZzx1EbSDfiathtU8NHs2TrHWNPHQjCdnnAJU8hdB4fSKpaCd4i5ZvpiSrPLegXpdsb9bMmJM9kv3T -p x -1
